80650af5d37f250d123f591a4572504973ae67ad
Neil-Do <dotatthanhk60@gmail.com>
2019-12-20 13:31:19 +0700

    add iam top 50 dataset

---
+++ WrittenIdentificationCode/DeepWriter.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+
+
+__all__ = ['DeepWriter', 'deepwriter']
+
+class DeepWriter(nn.Module):
+
+    def __init__(self, num_classes=10):
+        super(DeepWriter, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=5, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(96, 256, kernel_size=3,stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(1024, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+def deepwriter(pretrained=False, progress=True, **kwargs):
+    r"""DeepWriter: DOI 10.1109/ICFHR.2016.105
+    """
+    model = DeepWriter(**kwargs)
+    return model
--- WrittenIdentificationCode/ex.py
+++ WrittenIdentificationCode/ex.py
@@ -1,11 +1,27 @@
-import cv2 as cv
-import numpy as np
+#import cv2 as cv
+import math
+from PIL import Image
+import random as rd
+
+img = Image.open('a01-000u-s00-00.png')
+img_name = 'a01-000u-s00-00.png'
+name, extension = img_name.split('.')
+w, h = img.size
+n = math.floor(w / 75)
+subimg = (0, 0, 113, 113)
+count = 0
+img.crop(subimg).save(name + '-' + str(count) + '.' + extension)
+
+for i in range(1, n + 1):
+    count += 1
+    upper_left = i * 75 + rd.randint(0, 38)
+    upper_left = min(upper_left, w - 113)
+    subimg = (upper_left, 0, upper_left + 113, 113)
+    img.crop(subimg).save(name + '-' + str(count) + '.' + extension)
+    if upper_left == w - 113:
+        break
 
 
 
-img = cv.imread('IMG/CS2.jpg')
-dimension = img.shape
-print(dimension)
-# cv.imshow("CS2", img)
 #
 # cv.imwrite("CS2_copy.jpg", img)
