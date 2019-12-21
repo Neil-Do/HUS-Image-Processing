5444b6b8a32e6d4e698a6006fefaf34bf87a5cfb
Neil-Do <dotatthanhk60@gmail.com>
2019-12-22 06:35:16 +0700

    delete redundant data file

---
+++ WrittenIdentificationCode/MiniDeepWriter.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+
+
+__all__ = ['DeepWriter', 'deepwriter']
+
+class DeepWriter(nn.Module):
+
+    def __init__(self, num_classes=10):
+        super(DeepWriter, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=5, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3,stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 96, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(96, 96, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(96, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(64 * 6 * 6, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(256, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+def deepwriter(pretrained=False, progress=True, **kwargs):
+    r"""DeepWriter: DOI 10.1109/ICFHR.2016.105
+    """
+    model = DeepWriter(**kwargs)
+    return model
+
+
+# Construct our model by instantiating the class defined above
+model = DeepWriter(50)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
